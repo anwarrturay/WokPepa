133e20e6a6f62d24b630cc61f82766c9caf6fa64
Anwarr Turay <anwarrturay@icloud.com>
2025-06-08 16:59:22 -0700

    h-screen

--- UI/ultraResume/src/views/components/Sidebar.jsx
+++ UI/ultraResume/src/views/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 	}
 
 	return (
-		<div className="flex h-screen font-montserrat relative top-14">
+		<div style={{ height: "calc(100vh - 60rem)"}} className="flex font-montserrat relative top-14">
 			<div
 			className={`bg-white text-white h-full transition-all duration-300 ${
 				open ? "w-60" : "w-16"
