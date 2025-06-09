d0bf6bbc996e6630156bda6297aca211df6dffe7
Anwarr Turay <anwarrturay@icloud.com>
2025-06-08 17:36:40 -0700

    changed the backend url

--- UI/ultraResume/src/views/components/MainContent.jsx
+++ UI/ultraResume/src/views/components/MainContent.jsx
@@ -4,7 +4,7 @@ const MainContent = () => {
   return (
     <section style={{height: 'calc(100vh - 60px)'}} className='flex flex-col items-center justify-center text-[#333333] font-montserrat'>
         <h1 className="font-bold text-lg w-[280px] text-center">Start a new resume with Us</h1>
-        <h3 className='text-sm font-medium relative top-5'>Build a new resume with us</h3>
+        <h3 className='text-sm font-medium relative top-2'>Build a new resume with us</h3>
         <div className="flex flex-col items-center justify-center xl:flex-row lg:flex-row md:flex-row">
             <Link to={'/create-new-resume'} className="flex flex-col items-center mt-10 cursor-pointer">
                 <div className="w-[50mm] h-[57mm] bg-white shadow-lg border border-gray-300 flex items-center justify-center rounded-md">
